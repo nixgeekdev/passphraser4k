da778b6df716278275227aa88da7557c6a414d4f
Jonas Gorauskas <jgorauskas@gmail.com>
2025-05-18 06:33:20 -0700

    test fixtures

---
+++ lib/src/test/kotlin/dev/nixgeek/passphrases/fixtures/Constants.kt
@@ -0,0 +1,11 @@
+package dev.nixgeek.passphrases.fixtures
+
+const val FAKE_DICE_FACES_SIX = 6
+const val FAKE_DICE_FACES_TWENTY = 20
+const val FAKE_WORDLIST_ID_FOO = "foo"
+const val FAKE_WORDLIST_ID_BAR = "bar"
+const val FAKE_WORDLIST_ID_BAZ = "baz"
+const val FAKE_WORDLIST_ID_QUX = "qux"
+const val FAKE_WORDLIST_FILE_EXTENSION = ".json"
+const val FAKE_WORDLIST_FILE_SUFFIX = ".wordlist$FAKE_WORDLIST_FILE_EXTENSION"
+const val FAKE_WORDLIST_FILE_REINHOLD = "dw.reinhold.wordlist$FAKE_WORDLIST_FILE_EXTENSION"
---
+++ lib/src/test/kotlin/dev/nixgeek/passphrases/fixtures/WordlistGenerator.kt
@@ -0,0 +1,26 @@
+package dev.nixgeek.passphrases.fixtures
+
+import dev.nixgeek.passphrases.Random
+import dev.nixgeek.passphrases.wordlists.Wordlists
+import kotlinx.serialization.ExperimentalSerializationApi
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.JsonElement
+import kotlinx.serialization.json.JsonObject
+import kotlinx.serialization.json.decodeFromStream
+import kotlinx.serialization.json.jsonObject
+
+object WordlistGenerator {
+    private val json = Json { prettyPrint = true }
+    private val wlFileNames = Wordlists.entries.map { it.file }
+    private val jsonCache = wlFileNames.associateWith { fileName -> loadFile(fileName) }
+
+    @OptIn(ExperimentalSerializationApi::class)
+    private fun loadFile(filename: String): JsonElement {
+        return json.decodeFromStream<JsonElement>(
+            Random::class.java.getResourceAsStream("/$filename")
+                ?: throw IllegalStateException("wordlist not found for filename: $filename"),
+        )
+    }
+
+    fun generate(id: String): JsonObject? = jsonCache["$id.json"]?.jsonObject
+}
